4f80399938642006dc65be0f6ed98c55b5a92d73
mEzAcRiS <mezacristian2006@gmail.com>
2026-05-21 13:28:40 -0600

    Fusión de LoginScreen con los nuevos componentes de Vista Verde 2

--- app/src/main/java/com/sisvv/mobile/network/dto/productos/ItemCarritoDto.kt
+++ app/src/main/java/com/sisvv/mobile/network/dto/productos/ItemCarritoDto.kt
@@ -1,5 +1,7 @@
 package com.sisvv.mobile.network.dto.productos
 
+import com.google.gson.annotations.SerializedName
+
 data class ItemCarritoDto(
     val producto: ProductoDto,
     @SerializedName("modificadores_seleccionados") val modificadoresSeleccionados: List<ModificadorDto> = emptyList(),
