66aa047a75393196d87cf5e87e298a564350ecbb
mEzAcRiS <mezacristian2006@gmail.com>
2026-06-04 09:32:54 -0600

    Conflicto git

--- app/src/main/java/com/example/sisvvapp/network/RetrofitClient.kt
+++ app/src/main/java/com/example/sisvvapp/network/RetrofitClient.kt
@@ -13,7 +13,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
-    internal const val BASE_URL = "https://happy-ends-feel.loca.lt/api/"
+    internal const val BASE_URL = "https://blue-ties-burn.loca.lt/api/"
 
     private var apiService: ApiService? = null
 
