d2338e081420d70a0101fa8289222772651ac29a
mEzAcRiS <mezacristian2006@gmail.com>
2026-06-16 11:54:04 -0600

    App V2

--- app/src/main/java/com/example/sisvvapp/network/RetrofitClient.kt
+++ app/src/main/java/com/example/sisvvapp/network/RetrofitClient.kt
@@ -17,7 +17,7 @@ import java.util.concurrent.TimeUnit
 
 object RetrofitClient {
 
-    internal const val BASE_URL = "https://cyan-parents-teach.loca.lt/api/"
+    internal const val BASE_URL = "https://heavy-rats-listen.loca.lt/api/"
 
     private var apiService: ApiService? = null
 
