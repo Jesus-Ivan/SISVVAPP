7153918fd4d7a07a7f88f3026103bc8b78ba0cd2
mEzAcRiS <mezacristian2006@gmail.com>
2026-06-18 13:16:09 -0600

    Fase 6 — Limpieza y Mantenibilidad

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,7 +59,6 @@ dependencies {
     implementation(libs.coil.compose)
     implementation(libs.androidx.compose.material3.window.size)
     implementation(libs.haze)
-    implementation("androidx.compose.material:material-icons-extended")
 
     // Retrofit
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
@@ -87,7 +86,6 @@ dependencies {
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
     // ViewModel for Compose
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
-    implementation("androidx.compose.material3:material3:1.3.0")
     testImplementation(libs.junit)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
